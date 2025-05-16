44cd0358c8ee6319e2619d713790968915f3622d
najeebabdi <najeebwarsame@gmail.com>
2025-05-16 21:06:09 +0200

    spread operator and arrow function update

--- script.js
+++ script.js
@@ -2,3 +2,23 @@
 const playlistQuotes = document.getElementById("playlist-quotes");
 const playButton = document.getElementById("play");
 const pauseButton = document.getElementById("pause");
+
+const nextButton = document.getElementById("next");
+const previousButton = document.getElementById("previous");
+const shuffleButton = document.getElementById("shuffle");
+
+// Array of Quote objects
+const allQuotes = [
+/*{
+    id: ?,
+    title: I prefer not to speak,
+    duration: 0:30,
+    src:    ,
+
+    Continue with the rest of the objects
+}
+*/
+];
+
+// Working with Web Audio API 
+const audio = new Audio();
